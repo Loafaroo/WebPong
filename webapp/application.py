a3a8c150adddd53261735edb858fbb426c98f3a7
Loafaroo <53982793+Loafaroo@users.noreply.github.com>
2021-01-03 15:19:14 -0600

    Add AI opponent

--- webapp/application.py
+++ webapp/application.py
@@ -26,19 +26,37 @@ class Logicstate:
         self.player2 = Player2(width - player2_h_offset, height - player2_h_offset)
         self.ball = Ball()
 
-        self.gameState = 'play'
-
-        self.ball.serve()
+        self.gameState = 'wait to join'
 
     def update(self, W, S, up, down, enter):
-        ls.player1.direction_listen(W, S)
-        ls.player2.direction_listen(up, down)
-
-        self.ball.listen(enter)
-
-        self.player1.update_movement()
-        self.player2.update_movement()
+        if not self.player1.ai:
+            self.player1.direction_listen(W, S)
+        if not self.player2.ai:
+            self.player2.direction_listen(up, down)
+        else:
+            if self.ball.x > width / 4 and self.ball.velocity[0] > 0:
+                self.player2.direction_ai(self.ball.y)
+            else:
+                self.player2.moving = None
+                
+        if self.gameState == 'play':
+            self.ball.listen(enter)
+        if self.player1.assigned:
+            self.player1.update_movement()
+        if self.player2.assigned:
+            self.player2.update_movement()
         self.ball.update(self.player1, self.player2)
+
+    def assign_players(self):
+        if not self.player1.assigned:
+            self.player1.assigned = True
+        if self.player1.assigned and not self.player2.assigned:
+            self.player2.assigned = True
+            self.player2.ai = True
+
+    def game_ready(self):
+        if self.player1.assigned and self.player2.assigned:
+            self.gameState = 'play'
 
 class Player:
     def __init__(self, x, y):
@@ -46,6 +64,9 @@ class Player:
         self.y = y
         self.velocity = 0
         self.moving = None
+
+        self.assigned = False
+        self.ai = False
 
     def update_movement(self):
         if self.moving != None:
@@ -65,6 +86,14 @@ class Player:
         else:
             self.moving = None
 
+    def direction_ai(self, ball_y):
+        if self.y + player_height *1/4 < ball_y:
+            self.moving = DOWN
+        elif self.y + player_height * 3/4 >= ball_y:
+            self.moving = UP
+        else:
+            self.moving = None
+
 class Player1(Player):
     def __init__(self, x, y):
         super().__init__(x, y)
@@ -78,9 +107,10 @@ class Ball(Player):
         self.x, self.y = width // 2, height // 2
         self.width = ball_width
         self.height = ball_height
+        self.velocity = [0,0]
 
     def serve(self):
-        self.velocity = [random.uniform(-4, 4), random.uniform(-1, 1)]
+        self.velocity = [random.choice([-4, 4]), random.uniform(-1, 1)]
 
     def listen(self, enter):
         if enter == 1:
@@ -93,8 +123,10 @@ class Ball(Player):
 
         if self.Player1_Collision(player1):
             self.velocity[0] = abs(self.velocity[0])
+            self.velocity[1] += player1.velocity /5
         if self.Player2_Collision(player2):
             self.velocity[0] = -abs(self.velocity[0])
+            self.velocity[1] += player2.velocity /5
             
         self.x = self.x + self.velocity[0]
         self.y = self.y + self.velocity[1]
@@ -106,14 +138,27 @@ class Ball(Player):
 
     def Player1_Collision(self, player1):
         if abs(self.x - (player1.x)) < player_width:
-            if self.y -self.width > player1.y and self.y < player1.y + player_height:
+            if self.y + self.width > player1.y and self.y < player1.y + player_height:
                 return True
     def Player2_Collision(self, player2):
         if abs((self.x)- (player2.x)) < player_width:
-            if self.y -self.width > player2.y and self.y < player2.y + player_height:
+            if self.y + self.width > player2.y and self.y < player2.y + player_height:
                 return True
             
 ls = Logicstate()
+
+
+
+@app.route("/joinserver")
+def join():
+    if not ls.player1.assigned and not ls.player2.assigned:
+        ls.__init__()
+    
+    ls.assign_players()
+
+    ls.game_ready()    
+
+    
 
 @app.route("/update")
 def update():
